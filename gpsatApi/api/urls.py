528f5573b3c74265c6c25c54a4a8bd5251b663b7
Macário Soares <macario.junior@gmail.com>
2017-11-28 10:34:00 -0300

    Finalizing rest api

--- gpsatApi/api/urls.py
+++ gpsatApi/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('api/', views.evaluations_list, name="home"),
+    path('api/evaluations/', views.Evaluations.as_view(), name="home"),
+    path('api/evaluations/<pk>/', views.EvaluationDetail.as_view(), name="home"),
 ]
--- gpsatApi/api/views.py
+++ gpsatApi/api/views.py
@@ -1,29 +1,47 @@
-from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from django.http import Http404
 from .models import Evaluation
 from .serializer import EvaluationSerializer
-
-
-from rest_framework import views, mixins, permissions, exceptions
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_mongoengine import viewsets
-from rest_framework import parsers, renderers
 
 
-
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-def evaluations_list(request):
-    if request.method == 'GET':
+class Evaluations(APIView):
+    def get(self, request, format=None):
         evaluations = Evaluation.objects.all()
         serializer = EvaluationSerializer(evaluations, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EvaluationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EvaluationDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Evaluation.objects.get(pk=pk)
+        except Evaluation.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        evaluations = self.get_object(pk)
+        serializer = EvaluationSerializer(evaluations, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        evaluation = self.get_object(pk)
+        serializer = EvaluationSerializer(evaluation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        evaluation = self.get_object(pk)
+        evaluation.delete()
+        return Response(status=status.HTTP_200_OK)
